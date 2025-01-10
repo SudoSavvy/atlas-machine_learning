2ef41c5c200085268df32afd0265f3a467c57f09
SudoSavvy <donovanjheape@gmail.com>
2025-01-10 14:13:53 -0600

    Plotting code problems

--- math/linear_algebra/12-bracin_the_elements.py
+++ math/linear_algebra/12-bracin_the_elements.py
@@ -1,10 +1,32 @@
 #!/usr/bin/env python3
+
 def np_elementwise(mat1, mat2):
-    # Perform element-wise operations manually without using numpy
-    add = [[mat1[i][j] + mat2[i][j] for j in range(len(mat1[0]))] for i in range(len(mat1))]
-    sub = [[mat1[i][j] - mat2[i][j] for j in range(len(mat1[0]))] for i in range(len(mat1))]
-    mul = [[mat1[i][j] * mat2[i][j] for j in range(len(mat1[0]))] for i in range(len(mat1))]
-    div = [[mat1[i][j] / mat2[i][j] for j in range(len(mat1[0]))] for i in range(len(mat1))]
+    """
+    Perform element-wise addition, subtraction, multiplication, and division
+    on two matrices (or a matrix and scalar) without using loops or conditionals.
+
+    Args:
+        mat1: A 2D matrix (list of lists).
+        mat2: A 2D matrix (list of lists) or scalar value.
+
+    Returns:
+        A tuple containing:
+            - element-wise sum
+            - element-wise difference
+            - element-wise product
+            - element-wise quotient
+    """
+
+    # Element-wise addition
+    add = [[x + y for x, y in zip(row1, row2)] for row1, row2 in zip(mat1, mat2)]
     
-    # Return the results as a tuple
+    # Element-wise subtraction
+    sub = [[x - y for x, y in zip(row1, row2)] for row1, row2 in zip(mat1, mat2)]
+    
+    # Element-wise multiplication
+    mul = [[x * y for x, y in zip(row1, row2)] for row1, row2 in zip(mat1, mat2)]
+    
+    # Element-wise division
+    div = [[x / y for x, y in zip(row1, row2)] for row1, row2 in zip(mat1, mat2)]
+
     return add, sub, mul, div
