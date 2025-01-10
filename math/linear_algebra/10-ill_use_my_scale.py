9034e453f623496f7bb039bb180911a98bded561
SudoSavvy <donovanjheape@gmail.com>
2025-01-10 13:05:36 -0600

    Linear algebra problems

---
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+def np_shape(matrix):
+    """Calculates the shape of a numpy.ndarray.
+    
+    Args:
+        matrix (numpy.ndarray): Input matrix.
+
+    Returns:
+        tuple: Shape of the matrix as a tuple of integers.
+    """
+    return matrix.shape
