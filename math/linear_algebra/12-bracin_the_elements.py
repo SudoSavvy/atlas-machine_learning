ba35ccca5a190361185193f98b0ccf612893ca79
SudoSavvy <donovanjheape@gmail.com>
2025-01-10 14:11:53 -0600

    Plotting code problems

--- math/linear_algebra/12-bracin_the_elements.py
+++ math/linear_algebra/12-bracin_the_elements.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
-def np_transpose(matrix):
-    """Transposes a numpy.ndarray.
+import numpy as np
 
-    Args:
-        matrix (numpy.ndarray): Input matrix to transpose.
-
-    Returns:
-        numpy.ndarray: Transposed matrix.
-    """
-    return matrix.T
+def np_elementwise(mat1, mat2):
+    # Perform element-wise operations on the two matrices
+    add = np.add(mat1, mat2)
+    sub = np.subtract(mat1, mat2)
+    mul = np.multiply(mat1, mat2)
+    div = np.divide(mat1, mat2)
+    
+    # Return the results as a tuple
+    return add, sub, mul, div
