35ceb4057b1b2ccb935dad8c76a4900ff0cff1ff
SudoSavvy <donovanjheape@gmail.com>
2025-01-10 14:32:59 -0600

    Plotting code problems

--- math/linear_algebra/10-ill_use_my_scale.py
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -1,9 +1,22 @@
 #!/usr/bin/env python3
+"""
+Module for calculating the shape of a NumPy ndarray.
+
+This module provides a function `np_shape` that returns the shape of a given 
+NumPy matrix as a tuple. The shape describes the number of rows and columns 
+in a 2D matrix or the dimensions of an N-dimensional array.
+
+Example:
+    mat = np.array([[1, 2], [3, 4]])
+    shape = np_shape(mat)
+    print(shape)  # Output: (2, 2)
+"""
+
 def np_shape(matrix):
 
-    
+
     """Calculates the shape of a numpy.ndarray.
-    
+
     Args:
         matrix (numpy.ndarray): Input matrix.
 
