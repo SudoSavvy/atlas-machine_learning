acbef5b66760791d5b36b0b88f285b21bc32f3a1
SudoSavvy <donovanjheape@gmail.com>
2025-01-10 14:36:00 -0600

    Plotting code problems

--- math/linear_algebra/9-let_the_butcher_slice_it.py
+++ math/linear_algebra/9-let_the_butcher_slice_it.py
@@ -1,20 +1,8 @@
 #!/usr/bin/env python3
 import numpy as np
-
-# Create the matrix
-matrix = np.array([[36, 14, 57, 82, -9, 10],
-                   [100, 109, -36, 7, 2, 443],
-                   [6, 54, 67, 3, 3, 1],
-                   [57, 82, 23, 72, 45, 60],
-                   [23, 72, 12, 21, 54, 44],
-                   [12, 5, 44, 30, 11, 67]])
-
-# Correct slicing
-mat1 = matrix[1:3]  # Middle two rows
-mat2 = matrix[:, 2:4]  # Middle two columns
-mat3 = matrix[3:, 3:]  # Bottom-right 3x3 square matrix
-
-# Printing the results
-print("The middle two rows of the matrix are:\n{}".format(mat1))
-print("The middle two columns of the matrix are:\n{}".format(mat2))
-print("The bottom-right, square, 3x3 matrix is:\n{}".format(mat3))
+matrix = np.array([[36, 14, 57, 82, -9, 10], [100, 109, -36, 7, 2, 443], [6, 54, 67, 3, 3, 1], 
+                   [57, 82, 23, 72, 45, 60], [23, 72, 12, 21, 54, 44], [12, 5, 44, 30, 11, 67]])
+mat1 = matrix[1:3]; mat2 = matrix[:, 2:4]; mat3 = matrix[3:, 3:]
+print(f"The middle two rows of the matrix are:\n{mat1}")
+print(f"The middle two columns of the matrix are:\n{mat2}")
+print(f"The bottom-right, square, 3x3 matrix is:\n{mat3}")
