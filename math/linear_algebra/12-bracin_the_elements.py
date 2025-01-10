aad34377ea4300144504eb0c7ce0a787c0475114
SudoSavvy <donovanjheape@gmail.com>
2025-01-10 14:12:51 -0600

    Plotting code problems

--- math/linear_algebra/12-bracin_the_elements.py
+++ math/linear_algebra/12-bracin_the_elements.py
@@ -1,12 +1,10 @@
 #!/usr/bin/env python3
-import numpy as np
-
 def np_elementwise(mat1, mat2):
-    # Perform element-wise operations on the two matrices
-    add = np.add(mat1, mat2)
-    sub = np.subtract(mat1, mat2)
-    mul = np.multiply(mat1, mat2)
-    div = np.divide(mat1, mat2)
+    # Perform element-wise operations manually without using numpy
+    add = [[mat1[i][j] + mat2[i][j] for j in range(len(mat1[0]))] for i in range(len(mat1))]
+    sub = [[mat1[i][j] - mat2[i][j] for j in range(len(mat1[0]))] for i in range(len(mat1))]
+    mul = [[mat1[i][j] * mat2[i][j] for j in range(len(mat1[0]))] for i in range(len(mat1))]
+    div = [[mat1[i][j] / mat2[i][j] for j in range(len(mat1[0]))] for i in range(len(mat1))]
     
     # Return the results as a tuple
     return add, sub, mul, div
