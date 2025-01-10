a3882b0dfc27e3fe2c4a889bce43ac67b967fc42
SudoSavvy <donovanjheape@gmail.com>
2025-01-10 14:06:45 -0600

    Plotting code problems

---
+++ math/linear_algebra/11-the_western_exchange.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+def np_transpose(matrix):
+    """Transposes a numpy.ndarray.
+
+    Args:
+        matrix (numpy.ndarray): Input matrix to transpose.
+
+    Returns:
+        numpy.ndarray: Transposed matrix.
+    """
+    return matrix.T
