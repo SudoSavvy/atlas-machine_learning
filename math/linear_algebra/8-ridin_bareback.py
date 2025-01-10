30039cc412f65befc30dc3028b1736282943148d
SudoSavvy <donovanjheape@gmail.com>
2025-01-10 14:42:44 -0600

    Plotting code problems

--- math/linear_algebra/8-ridin_bareback.py
+++ math/linear_algebra/8-ridin_bareback.py
@@ -2,7 +2,18 @@
 
 
 def mat_mul(mat1, mat2):
+  """
+    Multiplies two matrices.
 
+    Args:
+        mat1 (list of lists): The first matrix represented as a 2D list.
+        mat2 (list of lists): The second matrix represented as a 2D list.
+
+    Returns:
+        list of lists: The result of the matrix multiplication, or None if the matrices
+                       cannot be multiplied due to incompatible dimensions.
+    """
+  
     # Check if the number of columns in mat1 matches the number of rows in mat2
     if len(mat1[0]) != len(mat2):
         return None
