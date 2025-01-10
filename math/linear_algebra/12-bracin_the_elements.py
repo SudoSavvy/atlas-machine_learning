8c96ba12d24cf91e84aee0c39d50931904e46ff9
SudoSavvy <donovanjheape@gmail.com>
2025-01-10 14:28:32 -0600

    Plotting code problems

--- math/linear_algebra/12-bracin_the_elements.py
+++ math/linear_algebra/12-bracin_the_elements.py
@@ -1,4 +1,20 @@
 #!/usr/bin/env python3
+"""
+Module for performing element-wise matrix operations.
+
+This module provides a function `np_elementwise` to perform element-wise addition, 
+subtraction, multiplication, and division on two matrices (or a matrix and a scalar). 
+The operations are performed using NumPy without using loops or conditional statements.
+
+Example:
+    mat1 = np.array([[1, 2], [3, 4]])
+    mat2 = np.array([[5, 6], [7, 8]])
+    add, sub, mul, div = np_elementwise(mat1, mat2)
+    print("Add:", add)
+    print("Sub:", sub)
+    print("Mul:", mul)
+    print("Div:", div)
+"""
 
 def np_elementwise(mat1, mat2):
 
