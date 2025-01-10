b2a6db64e3bdf6b730f73cb0779400feda48fd44
SudoSavvy <donovanjheape@gmail.com>
2025-01-10 13:07:14 -0600

    Linear algebra problems

---
+++ math/linear_algebra/13-cats_got_your_tongue.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+import numpy as np
+
+def np_cat(mat1, mat2, axis=0):
+    """Concatenates two matrices along a specific axis.
+
+    Args:
+        mat1 (numpy.ndarray): The first matrix.
+        mat2 (numpy.ndarray): The second matrix.
+        axis (int): The axis along which the matrices will be concatenated.
+
+    Returns:
+        numpy.ndarray: The concatenated matrix.
+    """
+    return np.concatenate((mat1, mat2), axis=axis)
