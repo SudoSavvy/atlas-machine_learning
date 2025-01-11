5f692836149e63252ad751d7b1dc0b125daa7455
SudoSavvy <donovanjheape@gmail.com>
2025-01-11 14:27:05 -0600

    More scatter problems

--- math/plotting/4-frequency.py
+++ math/plotting/4-frequency.py
@@ -5,19 +5,14 @@ import matplotlib.pyplot as plt
 def frequency():
     np.random.seed(5)
     student_grades = np.random.normal(68, 15, 50)
-    plt.figure(figsize=(6.4, 4.8))
 
-    # Plot the histogram with the exact specifications
-    plt.hist(student_grades, bins=range(0, 101, 10), edgecolor='black', align='mid')
-    
-    # Add labels and title
+    # Plot the histogram
+    plt.hist(student_grades, bins=range(0, 101, 10), edgecolor='black')
+
+    # Add the required labels and title
     plt.xlabel('Grades')
     plt.ylabel('Number of Students')
     plt.title('Project A')
 
-    # Set exact limits to match the example
-    plt.xlim(0, 100)
-    plt.ylim(0, 30)
-
-    # Show the plot
+    # Display the plot
     plt.show()
