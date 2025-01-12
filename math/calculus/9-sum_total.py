f4fd104584e71ffb2e95319a5436ce6b5ff0caa3
SudoSavvy <donovanjheape@gmail.com>
2025-01-12 13:52:44 -0600

    Sigma Notation

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -2,7 +2,8 @@
 
 """
 Module: 9-sum_total
-Description: This module contains a function to calculate the sum of squares of the first n integers.
+Description: This module contains a function to calculate
+the sum of squares of the first n integers.
 The sum is calculated using the formula:
     sum = n * (n + 1) * (2 * n + 1) / 6
 """
@@ -13,10 +14,10 @@ def summation_i_squared(n):
     Function: summation_i_squared
     Description: Calculates the sum of squares of the first n integers.
     Formula used: n * (n + 1) * (2 * n + 1) / 6
-    
+
     Parameters:
     n (int): The number up to which the sum of squares is calculated.
-    
+
     Returns:
     int: The sum of squares of the integers from 1 to n.
     None: If the input n is not a positive integer.
