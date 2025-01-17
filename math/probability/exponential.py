93e907bd7bd7e895ba655f200553b75c1e567094
SudoSavvy <donovanjheape@gmail.com>
2025-01-17 14:16:10 -0600

    Probability code work

---
+++ math/probability/exponential.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+class Exponential:
+    def __init__(self, data=None, lambtha=1.):
+        """
+        Initialize the Exponential distribution.
+
+        :param data: list of data points (optional)
+        :param lambtha: expected number of occurrences in a given time frame
+        """
+        if data is None:
+            if lambtha <= 0:
+                raise ValueError("lambtha must be a positive value")
+            self.lambtha = float(lambtha)
+        else:
+            if not isinstance(data, list):
+                raise TypeError("data must be a list")
+            if len(data) < 2:
+                raise ValueError("data must contain multiple values")
+            # Estimate lambtha as the inverse of the mean of the data
+            self.lambtha = 1 / (sum(data) / len(data))
