5a4321daf67ca09c300d23d2eb51865bf4cbce59
SudoSavvy <donovanjheape@gmail.com>
2025-01-19 15:37:39 -0600

    probability code

--- math/probability/normal.py
+++ math/probability/normal.py
@@ -56,3 +56,34 @@ class Normal:
             float: The x-value corresponding to z.
         """
         return z * self.stddev + self.mean
+
+    def pdf(self, x):
+        """
+        Calculate the value of the PDF for a given x-value.
+
+        Args:
+            x (float): The x-value.
+
+        Returns:
+            float: The PDF value for x.
+        """
+        # Calculate the PDF using the formula:
+        # PDF(x) = (1 / (stddev * sqrt(2 * pi))) * exp(-((x - mean)^2) / (2 * stddev^2))
+        pi = 3.141592653589793
+        sqrt_2pi = (2 * pi) ** 0.5
+
+        coeff = 1 / (self.stddev * sqrt_2pi)
+        exponent = -((x - self.mean) ** 2) / (2 * self.stddev ** 2)
+
+        # Calculate exp(exponent) manually for precision
+        exp_value = 1
+        term = 1
+        n = 1
+        while True:
+            term *= exponent / n
+            exp_value += term
+            n += 1
+            if abs(term) < 1e-15:
+                break
+
+        return coeff * exp_value
