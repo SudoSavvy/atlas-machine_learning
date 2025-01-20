a6635553e7eefd383ba98d50815f39792801e4c8
SudoSavvy <donovanjheape@gmail.com>
2025-01-20 15:04:57 -0600

    probability code

--- math/probability/normal.py
+++ math/probability/normal.py
@@ -54,13 +54,19 @@ class Normal:
 
     def erf(self, x):
         """Calculate the error function for a given x-value."""
-        # Coefficients for the approximation
-        a = [0.254829592, -0.284496736, 1.421413741, -1.453152027, 1.061405429]
+        a1 = 0.254829592
+        a2 = -0.284496736
+        a3 = 1.421413741
+        a4 = -1.453152027
+        a5 = 1.061405429
+        p = 0.3275911
+
         sign = 1 if x >= 0 else -1
-        x = abs(x)
-        t = 1 / (1 + 0.3275911 * x)
-        poly = (((((a[4] * t + a[3]) * t) + a[2]) * t + a[1]) * t + a[0])
-        return sign * (1 - poly * self.exp(-x * x))
+        t = 1.0 / (1.0 + p * abs(x))
+
+        y = 1.0 - (((((a5 * t + a4) * t) + a3) * t + a2) * t + a1) * t * self.exp(-x * x)
+
+        return sign * y
 
     def cdf(self, x):
         """Calculate the value of the CDF for a given x-value."""
