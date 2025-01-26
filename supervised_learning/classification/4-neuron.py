64b8bbb8b3a8bc873dac364d28b64d63afe2e8d3
SudoSavvy <donovanjheape@gmail.com>
2025-01-26 12:27:20 -0600

    Neural newtworks coding

---
+++ supervised_learning/classification/4-neuron.py
@@ -0,0 +1,100 @@
+#!/usr/bin/env python3
+import numpy as np
+
+
+class Neuron:
+    """
+    Defines a single neuron performing binary classification.
+    """
+
+    def __init__(self, nx):
+        """
+        Initializes the neuron.
+
+        Args:
+            nx (int): The number of input features to the neuron.
+
+        Raises:
+            TypeError: If nx is not an integer.
+            ValueError: If nx is less than 1.
+        """
+        if not isinstance(nx, int):
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be positive")
+
+        # Private weights vector initialized using a random normal distribution
+        self.__W = np.random.randn(1, nx)
+        # Private bias initialized to 0
+        self.__b = 0
+        # Private activated output initialized to 0
+        self.__A = 0
+
+    @property
+    def W(self):
+        """Getter for the weights vector."""
+        return self.__W
+
+    @property
+    def b(self):
+        """Getter for the bias."""
+        return self.__b
+
+    @property
+    def A(self):
+        """Getter for the activated output."""
+        return self.__A
+
+    def forward_prop(self, X):
+        """
+        Calculates the forward propagation of the neuron.
+
+        Args:
+            X (numpy.ndarray): Input data of shape (nx, m), where
+                nx is the number of input features to the neuron
+                m is the number of examples
+
+        Returns:
+            numpy.ndarray: The updated private attribute __A.
+        """
+        Z = np.dot(self.__W, X) + self.__b
+        self.__A = 1 / (1 + np.exp(-Z))
+        return self.__A
+
+    def cost(self, Y, A):
+        """
+        Calculates the cost of the model using logistic regression.
+
+        Args:
+            Y (numpy.ndarray): Correct labels for the input data, shape (1, m).
+            A (numpy.ndarray): Activated output of the neuron for each
+            example, shape (1, m).
+
+        Returns:
+            float: The cost of the model.
+        """
+        m = Y.shape[1]
+        cost = -np.sum(Y * np.log(A) + (1 - Y) * np.log(1.0000001 - A)) / m
+        return cost
+
+    def evaluate(self, X, Y):
+        """
+        Evaluates the neuron’s predictions.
+
+        Args:
+            X (numpy.ndarray): Input data of shape (nx, m), where
+                nx is the number of input features to the neuron
+                m is the number of examples
+            Y (numpy.ndarray): Correct labels for the input data, shape (1, m).
+
+        Returns:
+            tuple: The neuron’s prediction and the cost of the network.
+                - prediction (numpy.ndarray): Predicted labels for
+                each example, shape (1, m),
+                  where label values are 1 if the output is >= 0.5, otherwise 0.
+                - cost (float): The cost of the model.
+        """
+        A = self.forward_prop(X)
+        cost = self.cost(Y, A)
+        prediction = np.where(A >= 0.5, 1, 0)
+        return prediction, cost
