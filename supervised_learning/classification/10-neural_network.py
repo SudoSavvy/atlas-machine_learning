d7f79ef66dfeb4c2a0d6f854c312e4c2aaf15594
SudoSavvy <donovanjheape@gmail.com>
2025-01-27 13:15:36 -0600

    More Neural Networks code

--- supervised_learning/classification/10-neural_network.py
+++ supervised_learning/classification/10-neural_network.py
@@ -81,10 +81,12 @@ class NeuralNetwork:
 
         Parameters:
         X (numpy.ndarray): Input data with shape (nx, m),
-                           where nx is the number of features and m is the number of examples.
+                           where nx is the number of features and m
+                           is the number of examples.
 
         Returns:
-        tuple: Activated outputs  the hidden layer (__A1) and the output neuron (__A2).
+        tuple: Activated outputs  the hidden layer (__A1) and the
+        output neuron (__A2).
         """
         # Calculate the activated output  the hidden layer
         Z1 = np.matmul(self.__W1, X) + self.__b1
