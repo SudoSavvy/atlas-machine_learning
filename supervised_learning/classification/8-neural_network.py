9c72dead61d76a82056b62bb7bc326a4aa82abf6
SudoSavvy <donovanjheape@gmail.com>
2025-01-27 12:49:49 -0600

    More Neural Networks code

--- supervised_learning/classification/8-neural_network.py
+++ supervised_learning/classification/8-neural_network.py
@@ -17,10 +17,10 @@ class NeuralNetwork:
         if nodes < 1:
             raise ValueError("nodes must be a positive integer")
         
-        # Initialize weights and biases for the hidden and output layer
-        self.W1 = np.random.randn(nodes, nx)  # Random initialization for the hidden layer
-        self.b1 = np.zeros((nodes, 1))        # Bias for the hidden layer (zeros)
-        self.A1 = np.zeros((nodes, 1))        # Activated output for the hidden layer (zeros)
-        self.W2 = np.random.randn(1, nodes)   # Random initialization for the output layer
-        self.b2 = np.zeros((1, 1))            # Bias for the output layer (zero)
-        self.A2 = np.zeros((1, 1))            # Activated output for the output layer (zeros)
+        # Initialize weights and biases the hidden and output layer
+        self.W1 = np.random.randn(nodes, nx)  # Random initialization  the hidden layer
+        self.b1 = np.zeros((nodes, 1))        # Bias  the hidden layer (zeros)
+        self.A1 = np.zeros((nodes, 1))        # Activated output  the hidden layer (zeros)
+        self.W2 = np.random.randn(1, nodes)   # Random initialization  the output layer
+        self.b2 = np.zeros((1, 1))            # Bias  the output layer (zero)
+        self.A2 = np.zeros((1, 1))            # Activated output  the output layer (zeros)
