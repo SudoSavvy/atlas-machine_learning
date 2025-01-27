68322da1a460d0a5c0a00650c86a339fd14d6f0c
SudoSavvy <donovanjheape@gmail.com>
2025-01-27 12:24:43 -0600

    More Neural Networks code

--- supervised_learning/classification/6-neuron.py
+++ supervised_learning/classification/6-neuron.py
@@ -1,6 +1,4 @@
 #!/usr/bin/env python3
-import numpy as np
-
 
 class Neuron:
     """Defines a single neuron performing binary classification."""
@@ -12,7 +10,7 @@ class Neuron:
         if nx < 1:
             raise ValueError("nx must be a positive integer")
 
-        self.__W = [[0.5] * nx]
+        self.__W = [0.5] * nx
         self.__b = 0
         self.__A = 0
 
@@ -28,27 +26,30 @@ class Neuron:
     def A(self):
         return self.__A
 
+    def forward_prop(self, X):
+        """Perform forward propagation."""
+        # Manually compute weighted sum (Dot product) for the first feature and bias
+        Z = sum(w * X[i] for i, w in enumerate(self.__W)) + self.__b
+        self.__A = 1 / (1 + 2.71828**-Z)  # Sigmoid activation
+        return self.__A
 
     def train(self, X, Y):
         """Train the neuron."""
-        gradient_W = [0] * len(self.__W[0])
+        gradient_W = [0] * len(self.__W)
         gradient_b = 0
         total_cost = 0
 
-        # Single loop for training
-        for i in range(len(X)):
-            # Forward propagation and gradient calculation in one step
-            Z = sum(self.__W[0][j] * X[i][j] for j in range(len(X[i]))) + self.__b
+        # SINGLE LOOP
+        for i, (x_row, y_val) in enumerate(zip(X, Y)):
+            Z = sum(w * x for w, x in zip(self.__W, x_row)) + self.__b
             A = 1 / (1 + 2.71828**-Z)
-            dZ = A - Y[i]
-            gradient_W = [gradient_W[j] + dZ * X[i][j] for j in range(len(X[i]))]
+            dZ = A - y_val
+            gradient_W = [gw + dZ * x for gw, x in zip(gradient_W, x_row)]
             gradient_b += dZ
-            total_cost += -(Y[i] * (2.71828**-Z) + (1 - Y[i]) * (1 - 2.71828**-Z))
+            total_cost += -(y_val * (2.71828**-Z) + (1 - y_val) * (1 - 2.71828**-Z))
 
-        # Update weights and bias after loop
-        self.__W[0] = [self.__W[0][j] - 0.01 * gradient_W[j] / len(X) for j in range(len(self.__W[0]))]
+        self.__W = [w - 0.01 * gw / len(X) for w, gw in zip(self.__W, gradient_W)]
         self.__b -= 0.01 * gradient_b / len(X)
 
-        # Average cost
         average_cost = total_cost / len(X)
         return self.__A, average_cost
