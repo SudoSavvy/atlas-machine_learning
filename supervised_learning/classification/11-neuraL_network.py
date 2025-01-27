14e614e96dca33eee9d83c3e4ef9645c6f634575
SudoSavvy <donovanjheape@gmail.com>
2025-01-27 13:20:47 -0600

    More Neural Networks code

--- supervised_learning/classification/11-neuraL_network.py
+++ supervised_learning/classification/11-neuraL_network.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 """
-Defines a neural network with one hidden layer  binary classification.
+Defines a neural network with one hidden layer for binary classification.
 """
+
 import numpy as np
 
 
@@ -35,43 +36,43 @@ class NeuralNetwork:
             raise ValueError("nodes must be a positive integer")
 
         # Hidden layer
-        self.__W1 = np.random.randn(nodes, nx)  # Weight matrix  hidden layer
-        self.__b1 = np.zeros((nodes, 1))  # Bias vector  hidden layer
-        self.__A1 = 0  # Activated output  hidden layer
+        self.__W1 = np.random.randn(nodes, nx)  # Weight matrix for hidden layer
+        self.__b1 = np.zeros((nodes, 1))  # Bias vector for hidden layer
+        self.__A1 = 0  # Activated output for hidden layer
 
         # Output neuron
-        self.__W2 = np.random.randn(1, nodes)  # Weight vector  output neuron
-        self.__b2 = 0  # Bias  output neuron
-        self.__A2 = 0  # Activated output  output neuron (prediction)
+        self.__W2 = np.random.randn(1, nodes)  # Weight vector for output neuron
+        self.__b2 = 0  # Bias for output neuron
+        self.__A2 = 0  # Activated output for output neuron (prediction)
 
     @property
     def W1(self):
-        """Getter  W1."""
+        """Getter for W1."""
         return self.__W1
 
     @property
     def b1(self):
-        """Getter  b1."""
+        """Getter for b1."""
         return self.__b1
 
     @property
     def A1(self):
-        """Getter  A1."""
+        """Getter for A1."""
         return self.__A1
 
     @property
     def W2(self):
-        """Getter  W2."""
+        """Getter for W2."""
         return self.__W2
 
     @property
     def b2(self):
-        """Getter  b2."""
+        """Getter for b2."""
         return self.__b2
 
     @property
     def A2(self):
-        """Getter  A2."""
+        """Getter for A2."""
         return self.__A2
 
     def forward_prop(self, X):
@@ -83,13 +84,13 @@ class NeuralNetwork:
                            where nx is the number of features and m is the number of examples.
 
         Returns:
-        tuple: Activated outputs  the hidden layer (__A1) and the output neuron (__A2).
+        tuple: Activated outputs for the hidden layer (__A1) and the output neuron (__A2).
         """
-        # Calculate the activated output  the hidden layer
+        # Calculate the activated output for the hidden layer
         Z1 = np.matmul(self.__W1, X) + self.__b1
         self.__A1 = 1 / (1 + np.exp(-Z1))  # Sigmoid activation
 
-        # Calculate the activated output  the output neuron
+        # Calculate the activated output for the output neuron
         Z2 = np.matmul(self.__W2, self.__A1) + self.__b2
         self.__A2 = 1 / (1 + np.exp(-Z2))  # Sigmoid activation
 
@@ -100,8 +101,8 @@ class NeuralNetwork:
         Calculates the cost of the model using logistic regression.
 
         Parameters:
-        Y (numpy.ndarray): Correct labels  the input data with shape (1, m).
-        A (numpy.ndarray): Activated output of the neuron  each example with shape (1, m).
+        Y (numpy.ndarray): Correct labels for the input data with shape (1, m).
+        A (numpy.ndarray): Activated output of the neuron for each example with shape (1, m).
 
         Returns:
         float: The cost of the model.
