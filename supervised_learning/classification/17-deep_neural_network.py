b8c319ae773833ee029a02e7e07641039696f708
SudoSavvy <donovanjheape@gmail.com>
2025-01-29 14:01:17 -0600

    Even more Neural network code

--- supervised_learning/classification/17-deep_neural_network.py
+++ supervised_learning/classification/17-deep_neural_network.py
@@ -35,12 +35,6 @@ class DeepNeuralNetwork:
         if nx < 1:
             raise ValueError("nx must be a positive integer")
         
-        # Validate layers
-        if not isinstance(layers, list):
-            raise TypeError("layers must be a list of positive integers")
-        if not all(isinstance(x, int) and x > 0 for x in layers):
-            raise TypeError("layers must be a list of positive integers")
-        
         # Initialize private attributes
         self.__L = len(layers)  # Number of layers
         self.__cache = {}       # Cache for activations
