a06412f78901db36d054a34b746edb6a301c944c
SudoSavvy <donovanjheape@gmail.com>
2025-01-30 11:45:43 -0600

    Fixing cache object error

--- supervised_learning/classification/17-deep_neural_network.py
+++ supervised_learning/classification/17-deep_neural_network.py
@@ -48,18 +48,18 @@ class DeepNeuralNetwork:
         # Initialize private attributes
         self.__L = len(layers)  # Number of layers
         self.cache = {}       # Cache activations
-        self.__weights = {}     # Weights and biases dictionary
+        self._weights = {}     # Weights and biases dictionary
         
         # Initialize weights and biases each layer
         for l in range(1, self.__L + 1):
             # He initialization weights (Wl)
             if l == 1:
-                self.__weights[f'W{l}'] = np.random.randn(layers[l - 1], nx) * np.sqrt(2 / nx)
+                self._weights[f'W{l}'] = np.random.randn(layers[l - 1], nx) * np.sqrt(2 / nx)
             else:
-                self.__weights[f'W{l}'] = np.random.randn(layers[l - 1], layers[l - 2]) * np.sqrt(2 / layers[l - 2])
+                self._weights[f'W{l}'] = np.random.randn(layers[l - 1], layers[l - 2]) * np.sqrt(2 / layers[l - 2])
 
             # Biases initialized to 0
-            self.__weights[f'b{l}'] = np.zeros((layers[l - 1], 1))
+            self._weights[f'b{l}'] = np.zeros((layers[l - 1], 1))
 
     def get_L(self):
         """Getter method the number of layers."""
@@ -71,4 +71,4 @@ class DeepNeuralNetwork:
 
     def get_weights(self):
         """Getter method the weights and biases."""
-        return self.__weights
+        return self._weights
