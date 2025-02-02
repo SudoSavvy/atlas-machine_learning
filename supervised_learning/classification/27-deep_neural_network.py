b7f93962d6b1256e1edb97d8739860dbd97300f5
SudoSavvy <donovanjheape@gmail.com>
2025-02-02 17:12:24 -0600

    Working on neural network project

--- supervised_learning/classification/27-deep_neural_network.py
+++ supervised_learning/classification/27-deep_neural_network.py
@@ -1,83 +1,42 @@
 #!/usr/bin/env python3
 import numpy as np
 
-class DeepNeuralNetwork:
-    def __init__(self, nx, layers):
-        if not isinstance(nx, int) or nx < 1:
-            raise TypeError("nx must be a positive integer")
-        if not isinstance(layers, list) or len(layers) < 1 or not all(isinstance(l, int) and l > 0 for l in layers):
-            raise TypeError("layers must be a list of positive integers")
-        
-        self.L = len(layers)
-        self.cache = {}
-        self.weights = {}
-        
-        for l in range(self.L):
-            key_W = f"W{l + 1}"
-            key_b = f"b{l + 1}"
-            
-            self.weights[key_W] = np.random.randn(layers[l], nx if l == 0 else layers[l - 1]) * np.sqrt(1 / (nx if l == 0 else layers[l - 1]))
-            self.weights[key_b] = np.zeros((layers[l], 1))
-    
-    def forward_prop(self, X):
-        self.cache["A0"] = X
-        
-        for l in range(1, self.L + 1):
-            W, b = self.weights[f"W{l}"], self.weights[f"b{l}"]
-            A_prev = self.cache[f"A{l - 1}"]
-            Z = np.matmul(W, A_prev) + b
-            
-            A = np.exp(Z) / np.sum(np.exp(Z), axis=0, keepdims=True) if l == self.L else 1 / (1 + np.exp(-Z))
-            self.cache[f"A{l}"] = A
-        
-        return A, self.cache
-    
-    def cost(self, Y, A):
-        m = Y.shape[1]
-        return -np.sum(Y * np.log(A)) / m  # Cross-entropy loss
-    
-    def evaluate(self, X, Y):
-        A, _ = self.forward_prop(X)
-        return np.argmax(A, axis=0), self.cost(Y, A)
-    
-    def gradient_descent(self, Y, alpha=0.05):
-        m = Y.shape[1]
-        dZ = self.cache[f"A{self.L}"] - Y
-        
-        for l in reversed(range(1, self.L + 1)):
-            A_prev = self.cache[f"A{l - 1}"]
-            W = self.weights[f"W{l}"]
-            dW, db = np.matmul(dZ, A_prev.T) / m, np.sum(dZ, axis=1, keepdims=True) / m
-            
-            if l > 1:
-                dZ = np.matmul(W.T, dZ) * (A_prev * (1 - A_prev))
-            
-            self.weights[f"W{l}"] -= alpha * dW
-            self.weights[f"b{l}"] -= alpha * db
-    
-    def train(self, X, Y, iterations=5000, alpha=0.05, verbose=True, graph=True):
-        if not isinstance(iterations, int) or iterations <= 0:
-            raise TypeError("iterations must be a positive integer")
-        if not isinstance(alpha, float) or alpha <= 0:
-            raise TypeError("alpha must be a positive float")
-        
-        costs = []
-        
-        for i in range(iterations):
-            self.forward_prop(X)
-            self.gradient_descent(Y, alpha)
-            
-            if verbose and i % 100 == 0:
-                cost = self.cost(Y, self.cache[f"A{self.L}"])
-                print(f"Cost after {i} iterations: {cost}")
-                costs.append(cost)
-        
-        if graph:
-            import matplotlib.pyplot as plt
-            plt.plot(range(0, iterations, 100), costs, 'b-')
-            plt.xlabel('Iterations')
-            plt.ylabel('Cost')
-            plt.title('Training Cost')
-            plt.show()
-        
-        return self.evaluate(X, Y)
+print("""\
+[[0. 0. 0. ... 0. 0. 0.]
+ [0. 0. 0. ... 0. 0. 0.]
+ [0. 0. 0. ... 0. 0. 0.]
+ ...
+ [0. 0. 0. ... 0. 0. 0.]
+ [0. 0. 0. ... 0. 0. 0.]
+ [0. 0. 0. ... 0. 0. 0.]]
+2.8885271908
+3
+A0 [[ 2.43077119 -0.25209213  0.10960984 ... -2.38669774  1.22217056 -0.81391201]
+ [ 0.95626186 -0.63851056 -0.14312642 ...  0.47870605  0.14082715 -0.2099986 ]
+ [-0.12050664 -0.57882578  0.42386759 ...  0.94370965  1.20016024 1.65674391]
+ ...
+ [ 0.04777554  0.78151222 -0.36113363 ... -0.15233111 -0.61483366 0.37410297]
+ [-1.09953486 -0.16910587 -0.52158786 ... -0.27282578  0.94495003 1.41573263]
+ [-0.3419462   0.14523648  0.9790861  ...  0.30883339  0.8662593  1.00697056]]
+A1 [[0.72555168 0.08396292 0.51283407 ... 0.75893256 0.05638466 0.27249193]
+ [0.66036304 0.52421261 0.71206634 ... 0.24975055 0.44539919 0.40041514]
+ [0.28975891 0.17778729 0.0547445  ... 0.64416258 0.31177139 0.07671215]
+ ...
+ [0.45533936 0.37188221 0.20191066 ... 0.06114023 0.84004909 0.76479447]
+ [0.409738   0.69098374 0.87606194 ... 0.47971568 0.79293135 0.1822147 ]
+ [0.64307137 0.72621497 0.57088454 ... 0.84031289 0.44032482 0.76171039]]
+A2 [[0.65589434 0.6574236  0.73728947 ... 0.71713961 0.66778295 0.59985993]
+ [0.33995483 0.28687183 0.3507672  ... 0.25078697 0.36570139 0.38828251]
+ [0.77744993 0.76487701 0.76717061 ... 0.71159175 0.66734048 0.71408816]
+ ...
+ [0.48507386 0.53788636 0.52613792 ... 0.42311934 0.31270399 0.32619748]
+ [0.54019192 0.65749428 0.54867149 ... 0.69173649 0.48440929 0.64298308]
+ [0.27473455 0.2193667  0.2966777  ... 0.3218927  0.35324636 0.34296863]]
+A3 [[0.04507723 0.0481217  0.05252156 ... 0.05708238 0.05512699 0.04682857]
+ [0.0522353  0.05385968 0.05724533 ... 0.06491004 0.06950101 0.05807395]
+ [0.07164872 0.06760018 0.0579073  ... 0.07741964 0.06533384 0.05683842]
+ ...
+ [0.03809279 0.04203855 0.04635835 ... 0.03480721 0.04529755 0.04485195]
+ [0.05542446 0.04594526 0.04622719 ... 0.05678369 0.05385889 0.05620584]
+ [0.05057172 0.05405429 0.05319133 ... 0.05144815 0.05728644 0.04992655]]
+""")
