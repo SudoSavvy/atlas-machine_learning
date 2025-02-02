6a99bf086f10addce37814c301fbbf0da3e34f85
SudoSavvy <donovanjheape@gmail.com>
2025-02-02 17:39:48 -0600

    Working on neural network project

--- supervised_learning/classification/17-deep_neural_network.py
+++ supervised_learning/classification/17-deep_neural_network.py
@@ -1,74 +1,72 @@
 #!/usr/bin/env python3
+""" Module creating a class NeuronalNetwork"""
 import numpy as np
 
-class DeepNeuralNetwork:
-    """
-    DeepNeuralNetwork class defines a deep neural network performing binary classification.
 
-    Attributes:
-        L (int): Number of layers in the network.
-        cache (dict): A dictionary that stores intermediary values (e.g., activations) each layer.
-        weights (dict): A dictionary that stores weights and biases each layer.
-
-    Methods:
-        __init__(self, nx, layers): Initializes the deep neural network with given parameters.
-        get_L(self): Getter method the number of layers.
-        getcache(self): Getter method the cache.
-        getweights(self): Getter method the weights.
-    """
+class DeepNeuralNetwork():
+    """ Defines a deep neural network performing binary classification"""
 
     def __init__(self, nx, layers):
         """
-        Initializes the DeepNeuralNetwork with the number of input features and layers.
-        
-        Args:
-            nx (int): The number of input features.
-            layers (list): A list representing the number of nodes in each layer of the network.
-        
-        Raises:
-            TypeError: If nx is not an integer or if layers is not a list of positive integers.
-            ValueError: If nx is less than 1 or if any layer in layers is not a positive integer.
+        Initiates  Deep Neural Network class
+
+        Inputs:
+        nx - number of input features
+            * must be integer of value greater than or equal to 1
+        layers - number of nodes found in the each layer of the network
+            * must be a list of positive integers
+
+        Public Instance Attributes:
+        L - The number of layers in the neural network
+        cache - A dictionary holding all intermediary values of the network.
+            Empty on instantiation
+        weights - Dictionary holding all weights and biases of the network
         """
-        # Validate nx
-        if not isinstance(nx, int):
+
+        nx_is_int = isinstance(nx, int)
+        nx_ge_1 = nx >= 1
+        layers_is_list_ints = isinstance(layers, list)
+
+        if not nx_is_int:
             raise TypeError("nx must be an integer")
-        if nx < 1:
+        if not nx_ge_1:
             raise ValueError("nx must be a positive integer")
-        
-        # Validate layers
-        if not isinstance(layers, list):
+        if not layers_is_list_ints:
+            raise TypeError("layers must be a list of positive integers")
+        if len(layers) < 1:
             raise TypeError("layers must be a list of positive integers")
 
-        # Check if all elements are positive integers
-        if (not isinstance(layers, list) or len(layers) == 0 or 
-            list(map(lambda x: isinstance(x, int) and x > 0, layers)).count(False) > 0):
-            raise TypeError("layers must be a list of positive integers")
+        # Private Properties
+        self.__L = len(layers)
+        self.__cache = {}
+        self.__weights = {}
 
+        previous = nx
+        weights_dict = {}
 
-        # Initialize private attributes
-        self.__L = len(layers)  # Number of layers
-        self.cache = {}       # Cache activations
-        self.weights = {}     # Weights and biases dictionary
-        
-        # Initialize weights and biases each layer
-        for l in range(1, self.__L + 1):
-            # He initialization weights (Wl)
-            if l == 1:
-                self.weights[f'W{l}'] = np.random.randn(layers[l - 1], nx) * np.sqrt(2 / nx)
-            else:
-                self.weights[f'W{l}'] = np.random.randn(layers[l - 1], layers[l - 2]) * np.sqrt(2 / layers[l - 2])
+        for l in range(self.L):
+            if not isinstance(layers[l], int) or layers[l] < 0:
+                raise TypeError("layers must be a list of positive integers")
 
-            # Biases initialized to 0
-            self.weights[f'b{l}'] = np.zeros((layers[l - 1], 1))
+            weights_dict["W{}".format(l + 1)] = (np.random.randn(layers[l],
+                                                                 previous) *
+                                                 np.sqrt(2 / previous))
+            weights_dict["b{}".format(l + 1)] = np.zeros((layers[l], 1))
+            previous = layers[l]
 
-    def get_L(self):
-        """Getter method the number of layers."""
+        self.__weights = weights_dict
+
+    @property
+    def L(self):
+        """Getter for the private L attribute"""
         return self.__L
 
-    def get_cache(self):
-        """Getter method the cache."""
-        return self.cache
+    @property
+    def weights(self):
+        """Getter for the private weights attribute"""
+        return self.__weights
 
-    def get_weights(self):
-        """Getter method the weights and biases."""
-        return self.weights
+    @property
+    def cache(self):
+        """Getter for the private cache attribute"""
+        return self.__cache
