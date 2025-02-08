8b3ca89fd317a79a2084978141edf6bee0aa9f93
SudoSavvy <donovanjheape@gmail.com>
2025-02-08 16:15:12 -0600

    Tensorflow project work

--- supervised_learning/tensorflow/7-evaluate.py
+++ supervised_learning/tensorflow/7-evaluate.py
@@ -5,11 +5,6 @@ Module for calculating accuracy, loss, training operation, training, and evaluat
 """
 
 import tensorflow.compat.v1 as tf
-create_placeholders = __import__('0-create_placeholders').create_placeholders
-forward_prop = __import__('2-forward_prop').forward_prop
-calculate_accuracy = __import__('3-calculate_accuracy').calculate_accuracy
-calculate_loss = __import__('4-calculate_loss').calculate_loss
-create_train_op = __import__('5-create_train_op').create_train_op
 
 def calculate_accuracy(y, y_pred):
     """
@@ -54,59 +49,6 @@ def create_train_op(loss, alpha):
     optimizer = tf.train.GradientDescentOptimizer(learning_rate=alpha)
     train_op = optimizer.minimize(loss)
     return train_op
-
-def train(X_train, Y_train, X_valid, Y_valid, layer_sizes, activations, alpha, iterations, save_path="/tmp/model.ckpt"):
-    """
-    Build, train, and save a neural network classifier.
-
-    Args:
-        X_train (np.ndarray): Training input data.
-        Y_train (np.ndarray): Training labels.
-        X_valid (np.ndarray): Validation input data.
-        Y_valid (np.ndarray): Validation labels.
-        layer_sizes (list): Number of nodes in each layer of the network.
-        activations (list): Activation functions for each layer of the network.
-        alpha (float): Learning rate.
-        iterations (int): Number of iterations to train over.
-        save_path (str): Path to save the model.
-
-    Returns:
-        str: The path where the model was saved.
-    """
-    x, y = create_placeholders(X_train.shape[1], Y_train.shape[1])
-    y_pred = forward_prop(x, layer_sizes, activations)
-    loss = calculate_loss(y, y_pred)
-    accuracy = calculate_accuracy(y, y_pred)
-    train_op = create_train_op(loss, alpha)
-    
-    tf.add_to_collection('x', x)
-    tf.add_to_collection('y', y)
-    tf.add_to_collection('y_pred', y_pred)
-    tf.add_to_collection('loss', loss)
-    tf.add_to_collection('accuracy', accuracy)
-    tf.add_to_collection('train_op', train_op)
-    
-    init = tf.global_variables_initializer()
-    saver = tf.train.Saver()
-    
-    with tf.Session() as sess:
-        sess.run(init)
-        for i in range(iterations + 1):
-            train_cost, train_acc = sess.run([loss, accuracy], feed_dict={x: X_train, y: Y_train})
-            valid_cost, valid_acc = sess.run([loss, accuracy], feed_dict={x: X_valid, y: Y_valid})
-            
-            if i % 100 == 0 or i == iterations:
-                print(f"After {i} iterations:")
-                print(f"\tTraining Cost: {train_cost}")
-                print(f"\tTraining Accuracy: {train_acc}")
-                print(f"\tValidation Cost: {valid_cost}")
-                print(f"\tValidation Accuracy: {valid_acc}")
-            
-            if i < iterations:
-                sess.run(train_op, feed_dict={x: X_train, y: Y_train})
-        
-        save_path = saver.save(sess, save_path)
-    return save_path
 
 def evaluate(X, Y, save_path):
     """
