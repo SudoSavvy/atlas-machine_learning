e7586edc5bc935605549b2fba5c766a92167b182
SudoSavvy <donovanjheape@gmail.com>
2025-02-15 14:18:51 -0600

    Optimization project work

--- supervised_learning/optimization/4-moving_average.py
+++ supervised_learning/optimization/4-moving_average.py
@@ -4,12 +4,12 @@ import numpy as np
 
 def moving_average(data, beta):
     """
-    Calculates the exponentially weighted moving average (EWMA) of a dataset 
+    Calculates the exponentially weighted moving average (EWMA) of a dataset
     using bias correction.
 
     Parameters:
     data (list): List of numerical values to compute the moving average of.
-    beta (float): Weight factor (0 < beta < 1) determining how much 
+    beta (float): Weight factor (0 < beta < 1) determining how much
                   previous values influence the moving average.
 
     Returns:
@@ -29,10 +29,10 @@ def moving_average(data, beta):
 if __name__ == "__main__":
     data = [
         145.0, 149.0, 173.0, 98.0, 99.0, 76.0, 69.0, 77.0, 79.0, 97.0,
-        72.0, 81.0, 79.0, 47.0, 99.0, 49.0, 97.0, 18.0, 59.0, 86.0, 
-        111.0, 158.0, 69.0, 135.0, 54.0, 141.0, 116.0, 99.0, 66.0, 
-        38.0, 125.0, 64.0, 94.0, 39.0, 99.0, 92.0, 119.0, 54.0, 65.0, 
-        24.0, 82.0, 48.0, 105.0, 72.0, 29.0, 36.0, 86.0, 67.0, 79.0, 
+        72.0, 81.0, 79.0, 47.0, 99.0, 49.0, 97.0, 18.0, 59.0, 86.0,
+        111.0, 158.0, 69.0, 135.0, 54.0, 141.0, 116.0, 99.0, 66.0,
+        38.0, 125.0, 64.0, 94.0, 39.0, 99.0, 92.0, 119.0, 54.0, 65.0,
+        24.0, 82.0, 48.0, 105.0, 72.0, 29.0, 36.0, 86.0, 67.0, 79.0,
         102.0, 76.0, 33.0, 83.0, 37.0, 59.0, 94.0
     ]
     beta = 0.9  # Given weight
