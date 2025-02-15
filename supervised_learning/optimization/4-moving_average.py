fcb077463a5a4754dcf55f7e7311daa666dddc5b
SudoSavvy <donovanjheape@gmail.com>
2025-02-15 14:16:25 -0600

    Optimization project work

--- supervised_learning/optimization/4-moving_average.py
+++ supervised_learning/optimization/4-moving_average.py
@@ -13,12 +13,12 @@ def moving_average(data, bea):
     Returns:
     list: A list containing the moving averages of data.
     """
-    v = 0
-    moving_averages = []
+    v = 0.0  # Initialize moving average variable
+    moving_averages = []  # List to store moving average values
 
     for t in range(1, len(data) + 1):
-        v = beta * v + (1 - beta) * data[t -1]
-        bias_correction = 1 - beta**t
-        moving_averages.append(v / bias_correction)
+        v = beta * v + (1 - beta) * data[t - 1]  # Compute weighted moving average
+        bias_correction = 1 - beta**t  # Compute bias correction factor
+        moving_averages.append(v / bias_correction)  # Apply correction and store result
 
     return moving_averages
