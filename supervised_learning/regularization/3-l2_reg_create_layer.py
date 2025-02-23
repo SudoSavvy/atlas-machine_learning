58aee62a76d601078f0ecf22d42a4bcbe31aca13
SudoSavvy <donovanjheape@gmail.com>
2025-02-23 16:26:55 -0600

    Regularization project work

---
+++ supervised_learning/regularization/3-l2_reg_create_layer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Here's some documentation"""
+import numpy as np
+
+
+def l2_reg_create_layer(prev, n, activation, lambtha):
+    """And some more documentation"""
+    return 0
