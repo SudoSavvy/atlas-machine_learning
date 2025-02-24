676645e0dac06b5cb5c45bd1e002bb3b1e94bcba
SudoSavvy <donovanjheape@gmail.com>
2025-02-24 13:38:13 -0600

    Regularization project work

--- supervised_learning/regularization/6-dropout_create_layer.py
+++ supervised_learning/regularization/6-dropout_create_layer.py
@@ -1,22 +1,33 @@
 #!/usr/bin/env python3
+"""Create a Layer with a Dropout Module"""
 import tensorflow as tf
 
+
 def dropout_create_layer(prev, n, activation, keep_prob, training=True):
+    """Function that creates a layer of a neural network using dropout:
+
+    prev = a tensor containing the output of the previous layer
+    n = the number of nodes the new layer should contain
+    activation = the activation function for the new layer
+    keep_prob = the probability that a node will be kept
+    training = a boolean indicating whether the model is in training mode
+
     """
-    Creates a layer of a neural network using dropout.
-    
-    Parameters:
-    prev (tf.Tensor): Tensor containing the output of the previous layer
-    n (int): Number of nodes the new layer should contain
-    activation (function): Activation function for the new layer
-    keep_prob (float): Probability that a node will be kept
-    training (bool): Whether the model is in training mode
-    
-    Returns:
-    tf.Tensor: Output of the new layer
-    """
-    initializer = tf.keras.initializers.VarianceScaling(mode='fan_avg')
-    layer = tf.keras.layers.Dense(units=n, activation=None, kernel_initializer=initializer)(prev)
-    if training:
-        layer = tf.keras.layers.Dropout(rate=1 - keep_prob)(layer, training=True)
-    return activation(layer)
+    # init the layer weights using variance scaling
+    init_weights = tf.keras.initializers.VarianceScaling(scale=2.0,
+                                                         mode="fan_avg")
+
+    # creates a layer
+    layer = tf.keras.layers.Dense(units=n,
+                                  activation=activation,
+                                  kernel_initializer=init_weights)
+
+    # apply layer to the prev layer
+    layer_out = layer(prev)
+
+    # apply dropout to the layer output
+    dropout_layer = tf.keras.layers.Dropout(rate=1 - keep_prob)
+    layer_out = dropout_layer(layer_out, training=training)
+
+    # returns the output
+    return (layer_out)
