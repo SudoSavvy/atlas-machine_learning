7477041c5fc42eb125503721b8b8ace915c7bde8
SudoSavvy <donovanjheape@gmail.com>
2025-02-24 13:24:37 -0600

    Regularization project work

--- supervised_learning/regularization/4-dropout_forward_prop.py
+++ supervised_learning/regularization/4-dropout_forward_prop.py
@@ -1,40 +1,48 @@
 #!/usr/bin/env python3
+"""Forward Propagation with Dropout Module"""
 import numpy as np
 
+
 def dropout_forward_prop(X, weights, L, keep_prob):
+    """Function that conducts forward propagation using Dropout:
+
+    X = a numpy.ndarray of shape (nx, m) containing the input data
+    for the network
+    nx = the number of input features
+    m = the number of data points
+    weights = a dictionary of the weights and biases of the neural network
+    L = the number of layers in the network
+    keep_prob = the probability that a node will be kept
+    -> All layers except the last should use the tanh activation function
+    -> The last layer should use the softmax activation function
+
     """
-    Conducts forward propagation using Dropout.
-    
-    Parameters:
-    X (np.ndarray): Input data of shape (nx, m)
-    weights (dict): Dictionary containing weights and biases of the network
-    L (int): Number of layers in the network
-    keep_prob (float): Probability of keeping a node active during dropout
-    
-    Returns:
-    dict: Dictionary containing outputs and dropout masks for each layer
-    """
-    cache = {"A0": X}
-    
-    for l in range(1, L + 1):
-        W = weights[f"W{l}"]
-        b = weights[f"b{l}"]
-        A_prev = cache[f"A{l - 1}"]
-        Z = np.dot(W, A_prev) + b
-        
-        if l == L:
-            # Softmax activation for the last layer
+    # init the cache with X
+    cache = {'A0': X}
+
+    # iterate through each layer
+    for layer in range(1, L + 1):
+        # Compute Z
+        Z = np.matmul(weights['W' + str(layer)],
+                      cache['A' + str(layer - 1)]) + weights['b' + str(layer)]
+
+        # If it's the last layer, then use the softmax activation function
+        if layer == L:
             exp_Z = np.exp(Z - np.max(Z, axis=0, keepdims=True))
-            A = exp_Z / np.sum(exp_Z, axis=0, keepdims=True)
+            cache['A' + str(layer)] = exp_Z / np.sum(exp_Z,
+                                                     axis=0,
+                                                     keepdims=True)
         else:
-            # ReLU activation function for hidden layers
-            A = np.maximum(0, Z)
-            # Dropout mask
-            D = (np.random.rand(*A.shape) < keep_prob).astype(float)
-            A *= D  # Apply dropout
-            A /= keep_prob  # Scale activation values
-            cache[f"D{l}"] = D  # Store dropout mask
-        
-        cache[f"A{l}"] = A  # Store activation output
-    
-    return cache
+            # If not then use the tanh activation function
+            A = np.tanh(Z)
+
+            # Add the dropout mask and use it in the output, then scale
+            D = (np.random.rand(*A.shape) < keep_prob).astype(int)
+            A = (A * D) / keep_prob
+
+            # Save the dropout mask, activation to the cache
+            cache['A' + str(layer)] = A
+            cache['D' + str(layer)] = D
+
+    # returns the cache containing activations and dropout mask
+    return (cache)
