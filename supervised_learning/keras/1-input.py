a7a6b18608d78a42b8a75621cb00a44eab5bae2f
SudoSavvy <donovanjheape@gmail.com>
2025-03-01 13:54:07 -0600

    keras project work

--- supervised_learning/keras/1-input.py
+++ supervised_learning/keras/1-input.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import tensorflow.keras as K
 
+
 def build_model(nx, layers, activations, lambtha, keep_prob):
     """
     Builds a neural network with Keras without using Sequential.
@@ -17,15 +18,15 @@ def build_model(nx, layers, activations, lambtha, keep_prob):
     """
     inputs = K.Input(shape=(nx,))
     x = inputs
-    
+
     for i in range(len(layers)):
         x = K.layers.Dense(
             layers[i], activation=activations[i],
             kernel_regularizer=K.regularizers.l2(lambtha)
         )(x)
-        
+
         if i < len(layers) - 1:  # Dropout is not applied to the last layer
             x = K.layers.Dropout(1 - keep_prob)(x)
-    
+
     model = K.Model(inputs=inputs, outputs=x)
     return model
