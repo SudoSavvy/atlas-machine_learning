e6a4178c663107752571c15a4fb6e353428984c4
SudoSavvy <donovanjheape@gmail.com>
2025-05-18 18:26:02 -0500

    Advanced Linear Algebra project work

--- unsupervised_learning/dimensionality_reduction/0-pca.py
+++ unsupervised_learning/dimensionality_reduction/0-pca.py
@@ -13,7 +13,11 @@ def pca(X, var=0.95):
     cumulative_var = np.cumsum(explained_var_ratios)
     
     # Find the smallest number of components that explain at least 'var' variance
+    # Handle the case where all components are needed
     k = np.argmax(cumulative_var >= var) + 1
+    # Ensure that if all components are needed, k is set correctly
+    if k == 1 and cumulative_var[0] < var:
+        k = len(cumulative_var)
     
     # Extract the top k components (rows of Vt) and transpose to get the weights matrix
     W = Vt[:k, :].T
