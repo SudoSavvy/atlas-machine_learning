ba6d462e1eaf15f82f2dc8a1ab29835d67afee20
SudoSavvy <donovanjheape@gmail.com>
2025-05-18 17:07:54 -0500

    Advanced Linear Algebra project work

--- unsupervised_learning/dimensionality_reduction/0-pca.py
+++ unsupervised_learning/dimensionality_reduction/0-pca.py
@@ -3,53 +3,25 @@ import numpy as np
 
 def pca(X, var=1.0):
     """Performs PCA on a dataset X to maintain the specified variance `var`"""
-    # Compute the covariance matrix
-    cov = np.matmul(X.T, X) / (X.shape[0] - 1)
-
-    # Compute eigenvalues and eigenvectors
-    eigvals, eigvecs = np.linalg.eigh(cov)
-
-    # Sort eigenvalues and eigenvectors in descending order
-    sorted_indices = np.argsort(eigvals)[::-1]
-    eigvals_sorted = eigvals[sorted_indices]
-    eigvecs_sorted = eigvecs[:, sorted_indices]
-
-    # Compute the cumulative variance
-    total_variance = np.sum(eigvals_sorted)
-    cumulative_variance = np.cumsum(eigvals_sorted) / total_variance
-
-    # Determine the number of components needed to reach desired variance
-    num_components = np.searchsorted(cumulative_variance, var) + 1
-
-    # Select the corresponding eigenvectors (principal components)
-    W = eigvecs_sorted[:, :num_components]
-
-    return W
-#!/usr/bin/env python3
-import numpy as np
-
-def pca(X, var=1.0):
-    """Performs PCA on a dataset X to maintain the specified variance `var`"""
-    # Compute the mean and center the data
-    X_mean = X - np.mean(X, axis=0)
+    # Center the data
+    X_centered = X - np.mean(X, axis=0)
 
     # Compute the covariance matrix
-    cov = np.matmul(X_mean.T, X_mean) / (X.shape[0] - 1)
+    cov_matrix = np.matmul(X_centered.T, X_centered) / (X.shape[0] - 1)
 
     # Compute eigenvalues and eigenvectors
-    eigvals, eigvecs = np.linalg.eigh(cov)
+    eigvals, eigvecs = np.linalg.eigh(cov_matrix)
 
     # Sort eigenvalues and eigenvectors in descending order
-    sorted_indices = np.argsort(eigvals)[::-1]
-    eigvals_sorted = eigvals[sorted_indices]
-    eigvecs_sorted = eigvecs[:, sorted_indices]
+    idx = np.argsort(eigvals)[::-1]
+    eigvals = eigvals[idx]
+    eigvecs = eigvecs[:, idx]
 
-    # Compute cumulative explained variance
-    total_variance = np.sum(eigvals_sorted)
-    cumulative_variance = np.cumsum(eigvals_sorted) / total_variance
+    # Compute cumulative variance ratio
+    cumulative_variance = np.cumsum(eigvals) / np.sum(eigvals)
 
-    # Select number of components to maintain the desired variance
-    num_components = np.searchsorted(cumulative_variance, var) + 1
+    # Determine the number of components to reach desired variance
+    k = np.searchsorted(cumulative_variance, var) + 1
 
-    # Return the selected eigenvectors
-    return eigvecs_sorted[:, :num_components]
+    # Select top-k eigenvectors
+    return eigvecs[:, :k]
