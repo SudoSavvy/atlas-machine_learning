ee4f1fd677d9a2f6bee98bc6920fc48634c6d344
SudoSavvy <donovanjheape@gmail.com>
2025-05-18 17:22:37 -0500

    Advanced Linear Algebra project work

--- unsupervised_learning/dimensionality_reduction/0-pca.py
+++ unsupervised_learning/dimensionality_reduction/0-pca.py
@@ -2,26 +2,20 @@
 import numpy as np
 
 def pca(X, var=1.0):
-    """Performs PCA on dataset X to retain the specified variance `var`"""
+    """Perform PCA on X, keeping enough components to retain variance 'var'"""
     # Center the data
     X_centered = X - np.mean(X, axis=0)
-
     # Covariance matrix
     cov = np.matmul(X_centered.T, X_centered) / (X.shape[0] - 1)
-
-    # Eigenvalues and eigenvectors
+    # Eigen decomposition
     eigvals, eigvecs = np.linalg.eigh(cov)
-
-    # Sort in descending order
-    sorted_indices = np.argsort(eigvals)[::-1]
-    eigvals = eigvals[sorted_indices]
-    eigvecs = eigvecs[:, sorted_indices]
-
-    # Cumulative explained variance
-    cumulative_variance = np.cumsum(eigvals) / np.sum(eigvals)
-
-    # Components to retain the desired variance
-    k = np.searchsorted(cumulative_variance, var) + 1
-
-    # Return the first k principal components
+    # Sort descending
+    idx = np.argsort(eigvals)[::-1]
+    eigvals = eigvals[idx]
+    eigvecs = eigvecs[:, idx]
+    # Compute cumulative variance ratio
+    cumvar = np.cumsum(eigvals) / np.sum(eigvals)
+    # Number of components to keep
+    k = np.searchsorted(cumvar, var) + 1
+    # Return principal components
     return eigvecs[:, :k]
