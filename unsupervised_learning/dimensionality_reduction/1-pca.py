67d91d6e4fc5c8d8fb049e6bec3247d8223ebf5d
SudoSavvy <donovanjheape@gmail.com>
2025-05-18 19:27:49 -0500

    Advanced Linear Algebra project work

--- unsupervised_learning/dimensionality_reduction/1-pca.py
+++ unsupervised_learning/dimensionality_reduction/1-pca.py
@@ -6,13 +6,7 @@ import numpy as np
 
 def pca(X, ndim):
     """And some more documentation"""
-    cov_matrix = np.cov(X, rowvar=False)
-    eigenvals, eigenvects = np.linalg.eigh(cov_matrix)
-    sorted_idx = np.argsort(eigenvals)[::-1]
-    eigenvals = eigenvals[sorted_idx]
-    eigenvects = eigenvects[:, sorted_idx]
-    W = eigenvects[:, :ndim]
-    T = np.dot(X, W)
+    pass
     return T
 
 
