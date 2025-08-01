52a25134d07056955ffa0094d12c7cf0da66c6eb
SudoSavvy <donovanjheape@gmail.com>
2025-08-01 15:34:57 -0500

    Natural Language Processing - Word Embeddings project work

--- supervised_learning/word_embeddings/0-bag_of_words.py
+++ supervised_learning/word_embeddings/0-bag_of_words.py
@@ -6,6 +6,21 @@ import re
 
 
 def bag_of_words(sentences, vocab=None):
+    """
+    Creates a bag of words embedding matrix.
+
+    Args:
+        sentences (list of str): List of sentences to analyze.
+        vocab (list of str, optional): Vocabulary words to use. If None,
+            all words from sentences are used.
+
+    Returns:
+        tuple:
+            embeddings (numpy.ndarray): Shape (s, f) matrix where s is the number
+                of sentences and f is the number of features (vocab words).
+            features (numpy.ndarray): List of features (vocab words) used for
+                the embeddings.
+    """
     # Tokenize: convert to lowercase and split on word boundaries,
     # excluding 's' and similar
     tokenized = [re.findall(r'\b[a-z]{2,}\b', s.lower()) for s in sentences]
