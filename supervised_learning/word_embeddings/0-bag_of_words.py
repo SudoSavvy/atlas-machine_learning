04114f0b359d0363d1370870b44eab0330a01c38
SudoSavvy <donovanjheape@gmail.com>
2025-08-01 15:15:02 -0500

    Natural Language Processing - Word Embeddings project work

--- supervised_learning/word_embeddings/0-bag_of_words.py
+++ supervised_learning/word_embeddings/0-bag_of_words.py
@@ -1,24 +1,36 @@
+#!/usr/bin/env python3
 import numpy as np
 import re
 
-def bag_of_words(sentences):
-    vocab = set()
-    tokenized_sentences = []
-    
-    for sentence in sentences:
-        # Tokenize: keep only whole words, lowercase
-        tokens = re.findall(r'\b\w+\b', sentence.lower())
-        tokenized_sentences.append(tokens)
-        vocab.update(tokens)
-    
-    vocab = sorted(vocab)
+def bag_of_words(sentences, vocab=None):
+    """
+    Creates a bag-of-words embedding matrix.
+
+    Args:
+        sentences: list of sentences (strings)
+        vocab: optional list of vocabulary words. If None, uses all words in sentences.
+
+    Returns:
+        embeddings: numpy.ndarray of shape (s, f)
+        features: list of features used (vocabulary words)
+    """
+    tokenized_sentences = [
+        re.findall(r'\b\w+\b', sentence.lower())
+        for sentence in sentences
+    ]
+
+    if vocab is None:
+        unique_words = set()
+        for tokens in tokenized_sentences:
+            unique_words.update(tokens)
+        vocab = sorted(unique_words)
+
     word_to_index = {word: i for i, word in enumerate(vocab)}
-    
-    matrix = np.zeros((len(sentences), len(vocab)), dtype=int)
-    
+    embeddings = np.zeros((len(sentences), len(vocab)), dtype=int)
+
     for i, tokens in enumerate(tokenized_sentences):
         for token in tokens:
             if token in word_to_index:
-                matrix[i][word_to_index[token]] += 1
-    
-    return matrix, vocab
+                embeddings[i, word_to_index[token]] += 1
+
+    return embeddings, vocab
