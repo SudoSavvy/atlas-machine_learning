86e2615b9eebe3f821bae5ce19e3ed2c633b3d02
SudoSavvy <donovanjheape@gmail.com>
2025-08-01 16:44:40 -0500

    Natural Language Processing - Word Embeddings project work

--- supervised_learning/word_embeddings/4-fasttext.py
+++ supervised_learning/word_embeddings/4-fasttext.py
@@ -1,34 +1,44 @@
 #!/usr/bin/env python3
-"""
-Creates and trains a gensim FastText model
-"""
-from gensim.models import FastText
+"""FastText Module"""
+import gensim
 
 
-def fasttext_model(sentences, size=100, min_count=5, negative=5, window=5,
-                   cbow=True, iterations=5, seed=0, workers=1):
+def fasttext_model(sentences, vector_size=100, min_count=5, negative=5,
+                   window=5, cbow=True, epochs=5, seed=0, workers=1):
+    """Function that creates, builds and trains a genism fastText model:
+
+    sentences is a list of sentences to be trained on
+    vector_size is the dimensionality of the embedding layer
+    min_count is the minimum number of occurrences of a word for use in
+    training
+    window is the maximum distance between the current and predicted word
+    within a sentence
+    negative is the size of negative sampling
+    cbow is a boolean to determine the training type; True is for CBOW;
+    False is for Skip-gram
+    epochs is the number of iterations to train over
+    seed is the seed for the random number generator
+    workers is the number of worker threads to train the model
+    Returns: the trained model
+
     """
-    Creates and trains a gensim FastText model
-
-    Args:
-        sentences (list): A list of sentences to be trained on
-        size (int): The dimensionality of the embedding layer
-        min_count (int): The minimum number of occurrences of a word for use in training
-        window (int): The maximum distance between the current and predicted word within a sentence
-        negative (int): The size of negative sampling
-        cbow (bool): Boolean to determine the training type; True is for CBOW, False is for Skip-gram
-        iterations (int): The number of iterations over the corpus
-        seed (int): The seed for the random number generator
-        workers (int): The number of worker threads to train the model
-
-    Returns:
-        FastText: The trained FastText model
-    """
-    sg = 0 if cbow else 1
-
-    model = FastText(window=window, min_count=min_count, workers=workers,
-                     sg=sg, negative=negative, seed=seed)
+    # create the model with the settings
+    model = gensim.models.FastText(
+            sentences=sentences,
+            vector_size=vector_size,
+            window=window,
+            min_count=min_count,
+            negative=negative,
+            sg=0 if cbow else 1,
+            seed=seed,
+            workers=workers,
+            epochs=epochs
+    )
+    # build the model vocab
     model.build_vocab(sentences)
-    model.train(sentences, total_examples=len(sentences), epochs=iterations)
+    # train the model
+    model.train(sentences,
+                total_examples=model.corpus_count,
+                epochs=epochs)
 
     return model
