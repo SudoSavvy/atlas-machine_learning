38262d480b58d3fcb619b1195b7882451e290444
SudoSavvy <donovanjheape@gmail.com>
2025-08-01 16:14:48 -0500

    Natural Language Processing - Word Embeddings project work

--- supervised_learning/word_embeddings/2-word2vec.py
+++ supervised_learning/word_embeddings/2-word2vec.py
@@ -7,14 +7,17 @@ import gensim
 def word2vec_model(sentences, vector_size=100, min_count=5, window=5,
                    negative=5, cbow=True, epochs=5, seed=0, workers=1):
     """Creates and trains a Word2Vec model."""
-    return gensim.models.Word2Vec(
-        sentences=sentences,
+    model = gensim.models.Word2Vec(
         vector_size=vector_size,
         window=window,
         min_count=min_count,
         workers=workers,
         sg=0 if cbow else 1,
         negative=negative,
-        seed=seed,
-        epochs=epochs
+        seed=seed
     )
+
+    model.build_vocab(sentences)
+    model.train(sentences, total_examples=len(sentences), epochs=epochs)
+
+    return model
