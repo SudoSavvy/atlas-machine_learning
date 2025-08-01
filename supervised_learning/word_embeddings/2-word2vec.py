9d7059c95d4c7dfa48fa57a7326697a396321cd3
SudoSavvy <donovanjheape@gmail.com>
2025-08-01 16:49:51 -0500

    Natural Language Processing - Word Embeddings project work

--- supervised_learning/word_embeddings/2-word2vec.py
+++ supervised_learning/word_embeddings/2-word2vec.py
@@ -1,23 +1,44 @@
 #!/usr/bin/env python3
-"""Train a Word2Vec model using gensim."""
-
+"""Train Word2Vec Module"""
 import gensim
 
 
-def word2vec_model(sentences, vector_size=100, min_count=5, window=5,
-                   negative=5, cbow=True, epochs=5, seed=0, workers=1):
-    """Creates and trains a Word2Vec model."""
+def word2vec_model(sentences, vector_size=100, min_count=5,
+                   window=5, negative=5, cbow=True, epochs=5,
+                   seed=0, workers=1):
+    """Function that creates , builds and trains a gensim word2vec model:
+
+    sentences is a list of sentences to be trained on
+    vector_size is the dimensionality of the embedding layer
+    min_count is the minimum number of occurrences of a word for use in
+    training
+    window is the maximum distance between the current and predicted word
+    within a sentence
+    negative is the size of negative sampling
+    cbow is a boolean to determine the training type; True is for CBOW;
+    False is for Skip-gram
+    epochs is the number of iterations to train over
+    seed is the seed for the random number generator
+    workers is the number of worker threads to train the model
+    Returns: the trained model
+
+    """
+    # init the Word2Vec model with the given parameters
     model = gensim.models.Word2Vec(
+        sentences=sentences,
         vector_size=vector_size,
         window=window,
         min_count=min_count,
-        workers=workers,
-        sg=0 if cbow else 1,
         negative=negative,
-        seed=seed
+        sg=0 if cbow else 1,
+        seed=seed,
+        workers=workers
     )
 
+    # call and build
     model.build_vocab(sentences)
-    model.train(sentences, total_examples=len(sentences), epochs=epochs)
+    # train the model on the sentences for the given number of epochs
+    model.train(sentences, total_examples=model.corpus_count, epochs=epochs)
 
+    # return the trained Word2Vec model
     return model
