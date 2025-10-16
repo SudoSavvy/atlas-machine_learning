f2703d472430bdc8e74db442691c9caa6dffd35b
SudoSavvy <donovanjheape@gmail.com>
2025-10-15 21:44:48 -0500

    Pandas project work

--- pipeline/pandas/5-slice.py
+++ pipeline/pandas/5-slice.py
@@ -4,6 +4,7 @@ This module defines a function that extracts specific columns from a pandas
 DataFrame and selects every 60th row from those columns.
 """
 
+
 def slice(df):
     """
     Extracts the 'High', 'Low', 'Close', and 'Volume_(BTC)' columns from the
@@ -13,6 +14,7 @@ def slice(df):
     df (pd.DataFrame): A pandas DataFrame containing the required columns.
 
     Returns:
-    pd.DataFrame: A sliced DataFrame with every 60th row of the selected columns.
+    pd.DataFrame: A sliced DataFrame with every 60th row of the selected
+      columns.
     """
     return df[['High', 'Low', 'Close', 'Volume_(BTC)']].iloc[::60]
