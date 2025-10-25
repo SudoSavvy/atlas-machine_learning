497874cc126b8a250b54ccf1978457245126ca8f
SudoSavvy <donovanjheape@gmail.com>
2025-10-25 16:13:35 -0500

    Databases project work

--- pipeline/databases/12-rating_genres.sql
+++ pipeline/databases/12-rating_genres.sql
@@ -2,7 +2,7 @@
 -- Each record displays: tv_genres.name - rating sum.
 -- Results are sorted in descending order by rating.
 
-SELECT tv_genres.name AS genre, SUM(tv_show_ratings.rate) AS rating
+SELECT tv_genres.name AS name, SUM(tv_show_ratings.rate) AS rating
 FROM tv_genres
 JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
 JOIN tv_show_ratings ON tv_show_genres.show_id = tv_show_ratings.show_id
