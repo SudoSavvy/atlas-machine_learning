a6eddc61b317f0890ee211642f396bdc82a9802a
SudoSavvy <donovanjheape@gmail.com>
2025-10-27 13:43:03 -0500

    Databases project work

--- pipeline/databases/17-store.sql
+++ pipeline/databases/17-store.sql
@@ -1,15 +1,15 @@
 -- This script creates a trigger that decreases the quantity of an item
--- after a new order is added to the orders table.
+-- after a new order is inserted into the orders table.
 -- Quantity in the items table can be negative.
 
 DELIMITER $$
 
-CREATE TRIGGER decrease_quantity_after_order
+CREATE TRIGGER decrease_item_quantity
 AFTER INSERT ON orders
 FOR EACH ROW
 BEGIN
     UPDATE items
-    SET quantity = quantity - NEW.quantity
+    SET quantity = quantity - NEW.number
     WHERE id = NEW.item_id;
 END$$
 
