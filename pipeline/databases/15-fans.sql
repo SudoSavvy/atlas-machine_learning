f234603e021596dd1533ceb1d147726189742cb7
SudoSavvy <donovanjheape@gmail.com>
2025-10-27 13:45:15 -0500

    Databases project work

--- pipeline/databases/15-fans.sql
+++ pipeline/databases/15-fans.sql
@@ -1,7 +1,6 @@
--- This script ranks country origins of bands by the total number of non-unique fans.
--- It works on any database and handles any number of entries.
-
-SELECT origin, SUM(nb_fans) AS nb_fans
+-- Ranks country origins of bands by the number of fans
+SELECT origin, 
+       SUM(fans) AS nb_fans
 FROM metal_bands
 GROUP BY origin
 ORDER BY nb_fans DESC;
